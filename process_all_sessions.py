6e76adf9de0966e0dd5defa9881d396dc19fe9bd
Russ Poldrack <poldrack@gmail.com>
2021-07-25 14:30:19 -0700

    add -b flag

--- process_all_sessions.py
+++ process_all_sessions.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import os
-dicomdir = Path('/scratch/01329/poldrack/selftracking_2021/dicom/sub-01')
+dicomdir = Path('/home/poldrack/data/fmri-handbook-2e-data/dicom/sub-01')
 
 sessions = [os.path.basename(i) for i in dicomdir.glob('*')]
-DATAHOME='/scratch/01329/poldrack/selftracking_2021'
+DATAHOME='/home/poldrack/data/fmri-handbook-2e-data'
 DATADIR=os.path.join(DATAHOME, 'dicom')
 OUTDIR = os.path.join(DATAHOME, 'bids')
-CODE = os.path.join(DATAHOME, 'ds000031_conversion')
+CODE = os.path.join('/home/poldrack/Dropbox/code/ds000031_conversion')
 
 for s in sessions:
-   cmd = 'heudiconv -d %s/sub-{subject}/{session}/SCANS/*/DICOM/*dcm -s 01 -ss %s -c none -f %s/heuristic.py -o %s' % (DATADIR, s, CODE, OUTDIR)
+   cmd = 'heudiconv -d %s/sub-{subject}/{session}/SCANS/*/DICOM/*dcm -s 01 -ss %s -f %s/heuristic.py -b -o %s' % (DATADIR, s, CODE, OUTDIR)
    print(cmd)
